ecf16625f3a4bfe7b467300679471b67c0916370
Jessica Anderson <jessie.leigh30@gmail.com>
2019-04-17 16:40:10 -0600

    dashboard grid

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,1 +1,17 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+ 
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <Grid columns={2} stackable>
+        <Grid.Column>
+          <h1>Where the User info will go</h1>
+        </Grid.Column>
+      </Grid>
+     
+    )
+  }
+}
+
+export default Dashboard;
