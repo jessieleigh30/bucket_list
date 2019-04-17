5d8de452df429903ba5db500be5306696b4bb3db
Harlan Evans <harlanevans5@gmail.com>
2019-04-17 16:34:38 -0600

    add react-router-dom and browerrouter. add categories component

---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoMatch = () => (
+  <div>
+    Error 404 - No Match
+  </div>
+)
+
+export default NoMatch;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <App />, 
-document.getElementById('root')
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
