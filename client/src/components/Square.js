c7781cb5e2031f71c6ffd180a2c409211ca09d76
Jessica Anderson <jessie.leigh30@gmail.com>
2019-04-30 10:47:37 -0600

    react pose added challenge square created

---
+++ client/src/components/Square.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import posed from 'react-pose';
+import styled from 'styled-components';
+
+
+
+
+class ChallengeSquare extends React.Component {
+  state = { hovering: false }
+
+
+  render() {
+    return (
+      <Container>
+        <StyledSquare
+          pose={this.state.hovering ? "hovered" : "idle"}
+          onMouseEnter={() => this.setState({ hovering: true })}
+          onMouseLeave={() => this.setState({ hovering: false })}
+        />
+      </Container>
+    )
+  }
+};
+
+const Square = posed.div({
+  idle: { scale: 1 },
+  hovered: { scale: 1.5 }
+});
+
+const StyledSquare = styled(Square)`
+  width: 100px;
+  height: 50px;
+  background: #ea463e;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export default ChallengeSquare;
